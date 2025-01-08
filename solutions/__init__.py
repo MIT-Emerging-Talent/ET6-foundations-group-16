bf620b8458ca62c56767579a379738277ca33ed1
majdadel20 <majdadelalsoud@gmail.com>
2025-01-08 11:12:15 +0200

    Employee

--- solutions/__init__.py
+++ solutions/__init__.py
@@ -1,8 +1,8 @@
-ET6-foundations-group-16/ # type: ignore
-├── solutions/
-│   ├── __init__.py
-│   ├── employee.py
-│   └── tests/
-│       └── __init__.py
-│       └── test_employee.py
+# ET6-foundations-group-16/ # type: ignore
+#├── solutions/
+#│   ├── __init__.py
+#│   ├── employee.py
+#│   └── tests/
+#│       └── __init__.py
+#│       └── test_employee.py
 
