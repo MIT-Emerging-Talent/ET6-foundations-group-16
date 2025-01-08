4e1996b3946c90da19c2e08ee259460f0011f7b7
majdadel20 <majdadelalsoud@gmail.com>
2025-01-08 11:25:30 +0200

    Employees

--- solutions/Intersection_element.py
+++ solutions/Intersection_element.py
@@ -1,4 +1,3 @@
-
 def common_elements(list1, list2):
     # Convert both lists to sets to remove duplicates and find intersection
     set1 = set(list1)
@@ -8,9 +7,9 @@ def common_elements(list1, list2):
     # Convert the result back to a list
     return list(common)
 
+
 # Example usage
-list1 = [1, 2, 3, 4, 5, 6,7]
+list1 = [1, 2, 3, 4, 5, 6, 7]
 list2 = [4, 5, 6, 7, 8, 9]
 result = common_elements(list1, list2)
 print(result)  # Output: [4, 5, 6]
-
